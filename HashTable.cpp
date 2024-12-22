0f87c9ab253ffef739c224ae87e6999ea5b0bfe4
RamitS1493 <ramit.sachdev_ug2023@ashoka.edu.in>
2024-12-22 17:46:38 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -7,12 +7,10 @@ using namespace std;
 
 class HashTable {
 private:
-    static const int EMPTY   = -1;  
-    static const int DELETED = -2;  
-    int capacity;                 
-    std::vector<int> table;       
-    int size;                     
-    double load_factor_threshold = 0.8;
+    int capacity;                          
+    std::vector<int> table;               
+    int size;                              
+    double load_factor_threshold = 0.8;    
 
     
     int hashFunction(int key) {
@@ -20,76 +18,63 @@ private:
     }
 
 public:
-    HashTable(int initialSize) 
-        : capacity(initialSize), table(initialSize, EMPTY), size(0) {
+    
+    HashTable(int initialSize) : capacity(initialSize), table(initialSize, -1), size(0) {
     }
 
+    
     double loadFactor() {
         return static_cast<double>(size) / capacity;
     }
 
+    
     void insert(int value) {
         
         if (loadFactor() > load_factor_threshold) {
             resize();
         }
 
-        int hashIndex = hashFunction(value);
-        int firstDeletedIndex = -1;  
         int i = 0;
+        int keyIndex;
 
+        
         while (i < capacity) {
-            int probeIndex = (hashIndex + i * i) % capacity;
-
+            keyIndex = (hashFunction(value) + i * i) % capacity;
             
-            if (table[probeIndex] == EMPTY) {
-                if (firstDeletedIndex != -1) {
-                    
-                    table[firstDeletedIndex] = value;
-                } else {
-                    table[probeIndex] = value;
-                }
+            
+            if (table[keyIndex] == -1) {
+                table[keyIndex] = value;
                 size++;
                 return;
             }
             
-            else if (table[probeIndex] == DELETED) {
-                if (firstDeletedIndex == -1) {
-                    firstDeletedIndex = probeIndex;
-                }
-            }
-            
-            else if (table[probeIndex] == value) {
+            else if (table[keyIndex] == value) {
                 cout << "Duplicate key insertion is not allowed" << endl;
                 return;
             }
             i++;
         }
 
-        if (firstDeletedIndex != -1) {
-            
-            table[firstDeletedIndex] = value;
-            size++;
-        } else {
-            
-            cout << "Max probing limit reached!" << endl;
-        }
+        
+        cout << "Max probing limit reached!" << endl;
     }
 
+    
     void remove(int value) {
-        int hashIndex = hashFunction(value);
         int i = 0;
+        int keyIndex;
 
         while (i < capacity) {
-            int probeIndex = (hashIndex + i * i) % capacity;
-
-            if (table[probeIndex] == EMPTY) {
+            keyIndex = (hashFunction(value) + i * i) % capacity;
+            
+            
+            if (table[keyIndex] == -1) {
                 cout << "Element not found" << endl;
                 return;
             }
             
-            if (table[probeIndex] == value) {
-                table[probeIndex] = DELETED;
+            if (table[keyIndex] == value) {
+                table[keyIndex] = -1;
                 size--;
                 return;
             }
@@ -98,45 +83,51 @@ public:
         
         cout << "Element not found" << endl;
     }
+
     
     int search(int value) {
-        int hashIndex = hashFunction(value);
         int i = 0;
+        int keyIndex;
 
         while (i < capacity) {
-            int probeIndex = (hashIndex + i * i) % capacity;
-
-            if (table[probeIndex] == EMPTY) {
+            keyIndex = (hashFunction(value) + i * i) % capacity;
+            
+            
+            if (table[keyIndex] == -1) {
                 return -1;
             }
             
-            if (table[probeIndex] == value) {
-                return probeIndex;
+            if (table[keyIndex] == value) {
+                return keyIndex;
             }
-            
             i++;
         }
         return -1; 
     }
 
+    
     void resize() {
         
         std::vector<int> oldTable = table;
+
         
         int newCapacity = next_prime(capacity * 2);
 
-        std::vector<int> newTable(newCapacity, EMPTY);
+        
+        std::vector<int> newTable(newCapacity, -1);
         table = newTable;
         capacity = newCapacity;
         size = 0;
 
+        
         for (int val : oldTable) {
-            if (val != EMPTY && val != DELETED) {
+            if (val != -1) {
                 insert(val);
             }
         }
     }
 
+    
     bool is_prime(int num) {
         if (num <= 1) {
             return false;
@@ -149,6 +140,7 @@ public:
         return true;
     }
 
+    
     int next_prime(int n) {
         while (!is_prime(n)) {
             n++;
@@ -158,7 +150,7 @@ public:
 
     void printTable() {
         for (int i = 0; i < capacity; i++) {
-            if (table[i] == EMPTY || table[i] == DELETED) {
+            if (table[i] == -1) {
                 cout << "- ";
             } else {
                 cout << table[i] << " ";
