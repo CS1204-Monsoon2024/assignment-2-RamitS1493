f0fb951b475bd503cbf16a06f0c779693c219460
RamitS1493 <ramit.sachdev_ug2023@ashoka.edu.in>
2024-12-22 17:10:38 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,137 +1,23 @@
-#include "HashTable.cpp"
-
-int main() {
-    int initialSize = 7; 
-    HashTable ht(initialSize);
-
-    // Example test case
-    ht.insert(1);
-    ht.printTable();
-    ht.insert(6);
-    ht.printTable();
-    ht.insert(15);
-    ht.printTable(); 
-    ht.insert(25);
-    ht.printTable();
-    ht.remove(15);
-    ht.printTable();
-    ht.insert(29);  
-    ht.printTable(); 
-
-    int find = ht.search(22);
-    std::cout << "Found at:" << find << std::endl;
-
-    return 0;
-}
-
 #include <iostream>
-#include <list>
 #include <vector>
 #include <cmath>
-
 using namespace std;
 
 class HashTable {
 private:
-    int capacity;          
-    std::vector<int> table;
-    int size; 
+    int capacity;                
+    vector<int> table;           
+    int size;                    
     double load_factor_threshold = 0.8; 
 
-    
+    // the hashfunction
     int hashFunction(int key) {
         return key % capacity;
     }
 
-public:
-    
-    HashTable(int size) : capacity(size), table(size, -1), size(0) {}
-
-
-    double loadFactor() {
-        return static_cast<double>(size) / capacity;
-    }
-
-  
-    void insert(int value) {
-        if (loadFactor() > load_factor_threshold) {
-            resize();
-        }
-
-        int i = 0;
-        int key;
-
-        while (i < capacity) { 
-            key = (hashFunction(value) + i * i) % capacity;
-            if (table[key] == -1) {
-                table[key] = value; 
-                size++;
-                return;
-            } else if (table[key] == value) {
-                cout << "Duplicate key insertion is not allowed" << endl;
-                return;
-            }
-            i++;
-        }
-        cout << "Max probing limit reached!" << endl;
-    }
-
-   
-    void remove(int value) {
-        int i = 0;
-        int key;
-        while (i < capacity) { // Use capacity instead of max
-            key = (hashFunction(value) + i * i) % capacity;
-            if (table[key] == -1) {
-                cout << "Element not found" << endl;
-                return;
-            }
-            if (table[key] == value) {
-                table[key] = -1;
-                size--;
-                return;
-            }
-            i++;
-        }
-        cout << "Element not found" << endl;
-    }
-
-    
-    int search(int value) {
-        int i = 0;
-        int key;
-        while (i < capacity) { 
-            key = (hashFunction(value) + i * i) % capacity;
-            if (table[key] == -1) {
-                return -1; 
-            }
-            if (table[key] == value) {
-                return key; 
-            }
-            i++;
-        }
-        return -1;
-    }
-
-    void resize() {
-        std::vector<int> old_table = table;
-        int new_capacity = next_prime(capacity * 2);
-        std::vector<int> new_table(new_capacity, -1);
-        table = new_table; 
-        capacity = new_capacity;
-        size = 0;  
-
-        for (int value : old_table) {
-            if (value != -1) {
-                insert(value);
-            }
-        }
-    }
-        
+    // For checking the prime number
     bool is_prime(int num) {
-        if (num <= 1) {
-            return false;
-        }
+        if (num <= 1) return false;
         for (int i = 2; i <= sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
@@ -140,22 +26,113 @@ public:
         return true;
     }
 
+    // Finding next prime using while loop
     int next_prime(int n) {
         while (!is_prime(n)) {
-            n += 1;
+            n++;
         }
         return n;
     }
+    double loadFactor() {
+        return static_cast<double>(size) / capacity;
+    }
 
-    // Print the hash table
+    // Resizing and rehashing to fix
+    void resize() {
+        vector<int> old_table = table;
+        int new_capacity = next_prime(capacity * 2);
+        vector<int> new_table(new_capacity, -1);
+        table = new_table; 
+        capacity = new_capacity;
+        size = 0; 
+
+        for (int value : old_table) {
+            if (value != -1) {
+                insert(value);  
+            }
+        }
+    }
+
+public:
+    HashTable(int initialSize) : capacity(initialSize), table(initialSize, -1), size(0) {
+    }
+
+    void insert(int value) {
+        if (loadFactor() > load_factor_threshold) {
+            resize();
+        }
+        
+        int i = 0;
+        int probeIndex;
+
+        while (i < capacity) {
+            probeIndex = (hashFunction(value) + i*i) % capacity;
+            
+            if (table[probeIndex] == -1) {
+                table[probeIndex] = value;
+                size++;
+                return;
+            }
+            else if (table[probeIndex] == value) {
+                cout << "Duplicate key insertion is not allowed" << endl;
+                return;
+            }
+            i++;
+        }
+        
+        cout << "Max probing limit reached!" << endl;
+    }
+
+    void remove(int value) {
+        int i = 0;
+        int probeIndex;
+
+        while (i < capacity) {
+            probeIndex = (hashFunction(value) + i*i) % capacity;
+            
+            if (table[probeIndex] == -1) {
+                cout << "Element not found" << endl;
+                return;
+            }
+            
+            if (table[probeIndex] == value) {
+                table[probeIndex] = -1;
+                size--;
+                return;
+            }
+            i++;
+        }
+        cout << "Element not found" << endl;
+    }
+
+    int search(int value) {
+        int i = 0;
+        int probeIndex;
+        
+        while (i < capacity) {
+            probeIndex = (hashFunction(value) + i*i) % capacity;
+            
+            
+            if (table[probeIndex] == -1) {
+                return -1;
+            }
+            
+            if (table[probeIndex] == value) {
+                return probeIndex;
+            }
+            i++;
+        }
+        return -1;
+    }
+
     void printTable() {
         for (int i = 0; i < capacity; i++) {
             if (table[i] == -1) {
-                cout << "-" << " ";
+                cout << "- ";
             } else {
-                cout << table[i] << " "; 
+                cout << table[i] << " ";
             }
         }
-        cout << endl; 
+        cout << endl;
     }
-};\
+};
